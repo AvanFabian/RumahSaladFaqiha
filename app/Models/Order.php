dd57642e4ed3c0bb8c10b954f21ca8103fbbde14
AvanFabian <avanfabiandaniswara@gmail.com>
2024-03-08 10:40:56 +0700

    sesuaikan review+produk

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,15 +21,14 @@ class Order extends Model
         return $this->belongsTo(User::class, 'id_user');
     }
 
-
+    public function products()
+    {
+        return $this->belongsToMany(Produk::class, 'order_produk', 'order_id', 'produk_id')
+        // ->withPivot('harga', 'nama_produk', 'jumlah') // include 'jumlah'
+        ->withTimestamps();
+    }
     /** 
      * This function establishes a many-to-many relationship between the Order and Produk models. The belongsToMany method indicates that each Order can have multiple Produk and each Produk can belong to multiple Order.
      * The withPivot('harga', 'nama_produk') method indicates that the pivot table includes additional columns harga and nama_produk that store the price and name of the product for each order.
     */
-    public function products()
-    {
-        return $this->belongsToMany(Produk::class, 'order_produk', 'order_id', 'produk_id')
-                    ->withPivot('harga', 'nama_produk', 'jumlah') // include 'jumlah'
-                    ->withTimestamps();
-    }
 }
