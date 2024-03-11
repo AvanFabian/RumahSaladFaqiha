55704de0f05c1b3afd64c6d76c43399592b13221
AvanFabian <avanfabiandaniswara@gmail.com>
2024-03-11 13:51:25 +0700

    menusalad empty handling

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -63,12 +63,12 @@
          <div class="flex flex-col h-fit gap-y-6 mb-16">
             <div class="flex flex-col w-full text-center my-auto gap-y-9">
                <h3 class="text-6xl font-bold text-[#8D334E] drop-shadow-xl">Mengapa Salad Kami Istimewa?</h3>
-                  <p class="font-semibold text-base mx-auto w-3/4 text-[#57375D]">
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Voluptatibus qui ea ullam, enim tempora ipsum fuga alias quae
-                     ratione a officiis id temporibus autem? Quod nemo facilis
-                     cupiditate. Ex, vel?
-                  </p>
+               <p class="font-semibold text-base mx-auto w-3/4 text-[#57375D]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatibus qui ea ullam, enim tempora ipsum fuga alias quae
+                  ratione a officiis id temporibus autem? Quod nemo facilis
+                  cupiditate. Ex, vel?
+               </p>
             </div>
          </div>
       </div>
@@ -83,16 +83,6 @@
             <span class="text-[#ffababc7]">Salad</span>
          </h1>
       </div>
-
-      {{-- <div class="w-full flex flex-col gap-y-3 mx-auto text-center">
-         <img src="{{ asset('assets/images/blog-1.jpeg') }}" alt="" class="w-full h-[155px] lg:mx-auto" />
-         <h3 class="text-base text-[#57375D] font-bold capitalize">tasty and healty</h3>
-         <span class="text-xl text-[#57375D]">Rp 8.500,-</span>
-         <a href="#"
-            class="btn rounded-[30px]  text-white bg-[#D14D72] hover:bg-[#d14d72ce] lg:w-26 lg:mx-auto">
-            Tambah ke Keranjang
-         </a>
-      </div> --}}
 
       {{-- success message --}}
       @if (session('success'))
@@ -110,23 +100,38 @@
          </div>
       @endif
       <div class="grid grid-cols-2 lg:grid-cols-3 gap-y-8 lg:gap-y-24">
-         @foreach ($menusalad as $salad)
-            <div class="flex rounded-xl mx-auto w-full lg:w-[320px] pb-8 border-[5px] border-[#8d334e71] drop-shadow-xl">
+         @if (!$menusalad->isEmpty())
+            <div class="hidden lg:flex rounded-xl mx-auto w-full lg:w-[320px] pb-8 drop-shadow-xl">
+            </div>
+            <div class="flex rounded-xl mx-auto w-full ml-52 mt-16 lg:my-12 lg:w-[320px] pb-8 drop-shadow-xl">
                <div class="w-full flex flex-col gap-y-3 mx-auto text-center">
-                  <img src="{{ asset('storage/' . $salad->image) }}" alt="" class="w-full h-[155px] lg:mx-auto" />
-                  <h3 class="text-base text-[#57375D] font-bold capitalize">{{ $salad->title }}</h3>
-                  <h3 class="text-base text-[#57375D] font-bold capitalize">{{ $salad->desc }}</h3>
-                  <span class="text-xl text-[#57375D]">Rp {{ $salad->harga }},-</span>
-                  <form action="{{ route('cart.add') }}" method="POST">
-                     @csrf
-                     <input type="hidden" name="product_id" value="{{ $salad->id }}">
-                     <button type="submit" class="btn rounded-[30px]  text-white bg-[#D14D72] hover:bg-[#d14d72ce] lg:w-26 lg:mx-auto">
-                         Tambah ke Keranjang
-                     </button>
-                 </form>
+                  <h3 class="text-3xl text-[#57375D] font-bold capitalize">Maaf, Belum ada menu tersedia</h3>
                </div>
             </div>
-         @endforeach
+            <div class="hidden lg:flex rounded-xl mx-auto w-full lg:w-[320px] pb-8 drop-shadow-xl">
+            </div>
+         @else
+            @foreach ($menusalad as $salad)
+               <div
+                  class="flex rounded-xl mx-auto w-full lg:w-[320px] pb-8 border-[5px] border-[#8d334e71] drop-shadow-xl">
+                  <div class="w-full flex flex-col gap-y-3 mx-auto text-center">
+                     <img src="{{ asset('storage/' . $salad->image) }}" alt=""
+                        class="w-full h-[155px] lg:mx-auto" />
+                     <h3 class="text-base text-[#57375D] font-bold capitalize">{{ $salad->title }}</h3>
+                     <h3 class="text-base text-[#57375D] font-bold capitalize">{{ $salad->desc }}</h3>
+                     <span class="text-xl text-[#57375D]">Rp {{ $salad->harga }},-</span>
+                     <form action="{{ route('cart.add') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="product_id" value="{{ $salad->id }}">
+                        <button type="submit"
+                           class="btn rounded-[30px]  text-white bg-[#D14D72] hover:bg-[#d14d72ce] lg:w-26 lg:mx-auto">
+                           Tambah ke Keranjang
+                        </button>
+                     </form>
+                  </div>
+               </div>
+            @endforeach
+         @endif
       </div>
       </div>
    </section>
@@ -164,15 +169,15 @@
          </h1>
       </div>
 
-      <div class="grid grid-cols-2 lg:grid-cols-4 gap-y-8 lg:gap-y-24">
-         <div class="flex mx-auto px-4 py-6 lg:w-[320px] lg:px-8 lg:py-[48px] bg-[#fcc8d165] rounded-custom60">
+      <div class="grid grid-cols-2 lg:grid-cols-3 gap-y-8 lg:gap-y-24">
+         <div class="flex mx-auto px-4 py-6 lg:w-[320px] lg:px-8 lg:py-[48px] bg-[#fcc8d165] rounded lg:rounded-custom60">
             <div class="flex flex-col lg:gap-y-5 mx-auto text-center">
                <img src="{{ asset('assets/images/quote-img.png') }}" alt="" class="lg:w-16 mx-auto" />
-               <p class="text-[#8D334E] mb-7 font-semibold">
+               <p class="text-[#8D334E] text-xl mt-7 mb-7 font-semibold">
                   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
                   nulla sit libero nemo fuga sequi nobis? Necessitatibus aut"
                </p>
-               <h3 class="text-xl font-bold text-[#8D334E]">- Bu Dhia</h3>
+               <h3 class="text-2xl font-bold text-[#8D334E]">- Bu Dhia</h3>
             </div>
          </div>
       </div>
