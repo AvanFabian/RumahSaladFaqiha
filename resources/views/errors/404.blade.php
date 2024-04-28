e3368b3ae31db01ddd4dba7a78902cc3b3d46035
AvanFabian <avanfabiandaniswara@gmail.com>
2024-04-28 17:10:17 +0700

    feat: custom error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Halaman Tidak Ditemukan'))
+@section('code', '404')
+@section('message', __('Silahkan Masukkan URL yang Benar atau Kembali ke Halaman Sebelumnya'))
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Ups! Terjadi Kesalahan Server'))
+@section('code', '500')
+@section('message', __('Ups! Terjadi Kesalahan Server'))
